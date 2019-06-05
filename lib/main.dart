e4d06dce3a3a6f6cb5f94685b487a0ce76ceeb7a
Santiago Albisser <santiagoalbisser@hotmail.com>
2019-06-04 19:24:50 -0500

    Avance del login

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,20 @@
-hola mundo
+import 'package:flutter/material.dart';
+import 'package:higia/login_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      title: 'Hospital',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.lightBlue,
+      ),
+      home: LoginPage(),
+    );
+  }
+}
