375bc0e9e50325b871329837baf63fbf67b6bcb5
Santiago Albisser <santiagoalbisser@hotmail.com>
2019-06-05 11:24:39 -0500

    Login terminado

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 import 'home_page.dart';
 
@@ -7,8 +8,17 @@ class LoginPage extends StatefulWidget {
   _LoginPageState createState() => _LoginPageState();
 }
 
+
 class _LoginPageState extends State<LoginPage> {
+final _formKey = GlobalKey<FormState>();
+final userValue = new TextEditingController();
+final passwordValue = new TextEditingController();
+bool acceso= false;
+
+
+
   @override
+
   Widget build(BuildContext context) {
     final logo = Hero(
       tag: 'hero',
@@ -20,6 +30,20 @@ class _LoginPageState extends State<LoginPage> {
     );
 
     final email = TextFormField(
+      controller: userValue,
+      validator: (value){
+        if (value.isEmpty){
+          return 'try again';
+        }else if (value.contains('doctor')||value.contains('doctora')){
+          acceso= true;
+        }else if(value.contains('enfermero')|| value.contains('enfermera')){
+          acceso=true;
+        }else if(value.contains('administrador') || value.contains('administradora')) {
+           acceso=true;
+        }else {
+          return 'wrong id';
+        }
+      },
       keyboardType: TextInputType.text,
       autofocus: false,
       decoration: InputDecoration(
@@ -29,15 +53,24 @@ class _LoginPageState extends State<LoginPage> {
       ),
     );
     final password = TextFormField(
-      autofocus: false,
-      obscureText: true,
+      controller: passwordValue,
+      validator: (value){
+        if (value.isEmpty){
+          return 'try again';
+        } else if (value.length < 4){
+          return 'Error';
+        }
+      },
+      autofocus: false,    
+      obscureText: true,  
       decoration: InputDecoration(
         hintText: 'Password',
         contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
         border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
       ),
-    );
-    final loginButton = Padding(
+    );  
+    Widget _buildLoginButton(BuildContext context){
+      return Padding(
       padding: EdgeInsets.symmetric(vertical: 16.0),
       child: Material(
         borderRadius: BorderRadius.circular(30.0),
@@ -46,14 +79,18 @@ class _LoginPageState extends State<LoginPage> {
         child: MaterialButton(
           minWidth: 200.0,
           height: 42.0,
-          onPressed: () {
+          onPressed: () { 
+            //fetchPost(userValue,passwordValue); 
+            if (_formKey.currentState.validate()) {     
             Navigator.of(context).pushNamed(HomePage.tag);
+            }
           },
           color: Colors.lightBlueAccent,
           child: Text('Log In', style: TextStyle(color: Colors.white)),
         ),
       ),
     );
+    }
     final forgotLabel = FlatButton(
       child: Text('Forgot password?',
       style: TextStyle(color: Colors.black54),    
@@ -62,7 +99,10 @@ class _LoginPageState extends State<LoginPage> {
     );
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Center(
+      body: Builder(
+        builder: (context) => Center(
+          child: Form(
+            key: _formKey,
         child:ListView(
           shrinkWrap: true,
           padding: EdgeInsets.only(left: 24.0,right: 24.0),
@@ -73,11 +113,11 @@ class _LoginPageState extends State<LoginPage> {
             SizedBox(height: 8.0),
             password,
             SizedBox(height: 24.0),
-            loginButton,
+            _buildLoginButton(context),
             forgotLabel
-
           ],
-        ) ,)
+        ), )),
+      ),
     );
   }
 }
